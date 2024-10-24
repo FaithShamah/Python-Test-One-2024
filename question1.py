f526381461294792715c3fd828bd0f1f5d964d64
faithshamah <faithnamz23@gmail.com>
2024-10-24 01:45:22 -0700

    Python-Test-One-2024

---
+++ question1.py
@@ -0,0 +1,23 @@
+# Question 1(i)
+# Write a Python program to find the distance between two coordinate points (x1, y1) and (x2, y2).
+import math
+x1 = float(input("Enter the value of x1: "))
+x2 = float(input("Enter the value of x2: "))
+y1 = float(input("Enter the value of y1: "))
+y2 = float(input("Enter the value of y2: "))
+distance = math.sqrt((x2-x1)**2 + (y2-y1)**2 )
+print(f"The distance between two coordinate points is: {distance:.2f}")
+
+
+# Question 1(ii)
+# Write a Python program to find maximum between three numbers.
+number_one = 4
+number_two = 5
+number_three = 10
+max = (number_three)
+print(f"The maximum between three numbers is: {max}")
+
+
+
+
+
---
+++ question3.py
@@ -0,0 +1,23 @@
+# Question 3(i)
+#  Write a Python program that prompts a user to enter numbers. The process will repeat until
+#  the user enters 0. Finally, the program prints sum of the numbers entered by the user.
+
+number_one = float(input("Enter the first number: "))
+number_two = float(input("Enter the second number: "))
+number_three = float(input("Enter the third number: "))
+number_four = float(input("Enter the fourth number: "))
+numbers = (number_one,number_two,number_three,number_four)
+if numbers == 0:
+    print(f"Enter the correct value")
+else:
+    print(sum)
+sum = 0
+sum+=numbers(number_one,number_two,number_three,number_four)
+print(f"The sum of the numbers is: {sum}")
+
+
+
+
+# Question 3(ii)
+# Write a Python program to print all the numbers from 1 to 100 that are not divisible by 2
+
